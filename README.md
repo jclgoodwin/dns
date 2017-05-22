57106a8a0422809cb486bfe278456548019861e2
Josh Goodwin <j@joshuagoodw.in>
2017-05-22 23:29:09 +0100

    More, and IPv6

---
+++ README.md
@@ -0,0 +1,3 @@
+This is my DNS configuration for various domain names, using [DNSControl](https://stackexchange.github.io/dnscontrol/) which is rather great.
+
+Eventually I want to use multiple providers because why not. 
--- dnsconfig.js
+++ dnsconfig.js
@@ -1,9 +1,10 @@
-Providers:
+// Providers:
 
 var REG_NONE = NewRegistrar('none', 'NONE');
 var DNS_CLOUDFLARE = NewDnsProvider('cloudflare', 'CLOUDFLAREAPI');
 
 var SOLO = '85.159.213.230';
+var SOLO_6 = '2a01:7e00::f03c:91ff:febb:b640';
 var ASTROMEGA = '185.157.233.18';
 var ASTROMEGA_6 = '2a07:4580:b0d:e6::578b';
 
@@ -12,6 +13,8 @@ var ASTROMEGA_6 = '2a07:4580:b0d:e6::578b';
 D('bustimes.org.uk', REG_NONE, DnsProvider(DNS_CLOUDFLARE),
     A('@', SOLO),
     A('solo', SOLO),
+    AAAA('@', SOLO_6),
+    AAAA('solo', SOLO_6),
     CNAME('www', 'bustimes.org.uk.'),
     MX('@', 10, 'astromega.joshuagoodw.in.'),
     TXT('@', 'v=spf1 include:_spf.google.com a:bustimes.org.uk a:astromega.joshuagoodw.in -all')
@@ -32,12 +35,25 @@ D('alexisgoodwin.com', REG_NONE, DnsProvider(DNS_CLOUDFLARE),
 );
 
 D('joshuagoodw.in', REG_NONE, DnsProvider(DNS_CLOUDFLARE),
-    AAAA('astromega', ASTROMEGA_6),
-    AAAA('@', ASTROMEGA_6),
-    A('astromega', ASTROMEGA),
     A('@', ASTROMEGA),
+    A('astromega', ASTROMEGA),
+    AAAA('@', ASTROMEGA_6),
+    AAAA('astromega', ASTROMEGA_6),
     CNAME('*', 'joshuagoodw.in.'),
     CNAME('subtitles', 'bustimes.org.uk.'),
-    TXT('@', 'v=spf1 include:_spf.google.com a:solo.bustimes.org.uk a:astromega.joshuagoodw.in -all'),
-    MX('@', 10, 'joshuagoodw.in.')
+    MX('@', 10, 'joshuagoodw.in.'),
+    TXT('@', 'v=spf1 include:_spf.google.com a:solo.bustimes.org.uk a:astromega.joshuagoodw.in -all')
+);
+
+D('joshuagoodwin.com', REG_NONE, DnsProvider(DNS_CLOUDFLARE),
+    A('@', ASTROMEGA),
+    CNAME('*', 'joshuagoodw.in.'),
+    CNAME('www', 'joshuagoodw.in.'),
+    MX('@', 10, 'spool.mail.gandi.net.'),
+    MX('@', 50, 'fb.mail.gandi.net.')
+);
+
+D('supermarketmarket.co.uk', REG_NONE, DnsProvider(DNS_CLOUDFLARE),
+    A('@', '163.172.176.153'),
+    CNAME('www', 'supermarketmarket.co.uk.')
 );
