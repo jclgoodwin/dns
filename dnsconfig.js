63baf27f2b11d065c5b00afe13e49fca25c9d821
Josh Goodwin <j@joshuagoodw.in>
2020-02-29 08:30:45 +0000

    remove jonckheere, add mongoose

--- dnsconfig.js
+++ dnsconfig.js
@@ -6,22 +6,30 @@ var DNS_GANDI = NewDnsProvider('gandi', 'GANDI');
 
 var ASTROMEGA = '185.157.233.18';
 var ASTROMEGA_6 = '2a07:4580:b0d:e6::578b';
-var JONCKHEERE = '94.130.23.252';
-var JONCKHEERE_6 = '2a01:4f8:10b:1d03::2';
 
 var floating = '68.183.252.225';
 var rapta = '209.97.129.56';
 var rapta_6 = '2a03:b0c0:1:e0::3e1:3001';
+var mongoose = '68.183.34.157';
+var mongoose_6 = '2a03:b0c0:1:e0::5cf:b001';
 
-var STANDARD_SPF = TXT('@', 'v=spf1 include:_spf.google.com ip4:209.97.129.56 a:astromega.joshuagoodw.in a:jonckheere.joshuagoodw.in include:spf.messagingengine.com -all');
+var BUSTIMES_SPF = TXT('@', 'v=spf1 include:_spf.google.com include:spf.messagingengine.com a:rapta.bustimes.org a:solera.bustimes.org -all');
+var STANDARD_SPF = TXT('@', 'v=spf1 include:_spf.google.com include:spf.messagingengine.com a:astromega.joshuagoodw.in a:mongoose.bus.photos -all');
 
 // Domains:
+
+D('hygieneratings.co.uk', REG_NONE, DnsProvider(DNS_CLOUDFLARE),
+    A('@', '157.245.33.90'),
+    AAAA('@', '2a03:b0c0:1:e0::23e:e001'),
+    CNAME('www', 'hygieneratings.co.uk.'),
+    CNAME('basil', 'hygieneratings.co.uk.')
+);
 
 D('bustimes.org.uk', REG_NONE, DnsProvider(DNS_CLOUDFLARE),
     A('@', floating),
     AAAA('@', rapta_6),
     CNAME('www', 'bustimes.org.uk.'),
-    STANDARD_SPF,
+    BUSTIMES_SPF,
     MX('@', 10, 'in1-smtp.messagingengine.com.'),
     MX('@', 20, 'in2-smtp.messagingengine.com.'),
     CNAME('fm1._domainkey', 'fm1.bustimes.org.uk.dkim.fmhosted.com.'),
@@ -34,7 +42,7 @@ D('bustim.es', REG_NONE, DnsProvider(DNS_CLOUDFLARE),
     AAAA('@', rapta_6),
     CNAME('www', 'bustim.es.'),
     CNAME('api', 'd2mjppkdxq1joc.cloudfront.net.'),
-    STANDARD_SPF,
+    BUSTIMES_SPF,
     MX('@', 10, 'in1-smtp.messagingengine.com.'),
     MX('@', 20, 'in2-smtp.messagingengine.com.')
 );
@@ -44,15 +52,12 @@ D('bustimes.org', REG_NONE, DnsProvider(DNS_CLOUDFLARE),
     AAAA('@', rapta_6),
     A('rapta', rapta),
     AAAA('rapta', rapta_6),
-    A('solo', '142.93.37.175'),
     CNAME('www', 'bustimes.org.'),
-    A('berkhof', '167.71.104.162'),
+    A('solera', '142.93.40.224'),
     TXT('@', 'google-site-verification=Qn-hbgT7WIIN7YznRuYwwkn8rvWasjYL-mctHxLVDVo'),
-    STANDARD_SPF,
+    BUSTIMES_SPF,
     MX('@', 10, 'in1-smtp.messagingengine.com.'),
-    MX('@', 20, 'in2-smtp.messagingengine.com.'),
-    A('maps', JONCKHEERE, {cloudflare_proxy: 'on'}),
-    AAAA('maps', JONCKHEERE_6, {cloudflare_proxy: 'on'})
+    MX('@', 20, 'in2-smtp.messagingengine.com.')
 );
 
 D('bustimes.io', REG_NONE, DnsProvider(DNS_CLOUDFLARE),
@@ -60,7 +65,7 @@ D('bustimes.io', REG_NONE, DnsProvider(DNS_CLOUDFLARE),
     AAAA('@', rapta_6),
     CNAME('www', 'bustimes.io.'),
     TXT('@', 'google-site-verification=uLP7Vt-vsgfEUmauz7KLEoTyZVwjMCytWxIRvA73oKQ'),
-    STANDARD_SPF,
+    BUSTIMES_SPF,
     MX('@', 10, 'in1-smtp.messagingengine.com.'),
     MX('@', 20, 'in2-smtp.messagingengine.com.')
 );
@@ -70,8 +75,6 @@ D('joshuagoodw.in', REG_NONE, DnsProvider(DNS_CLOUDFLARE),
     AAAA('@', ASTROMEGA_6),
     A('astromega', ASTROMEGA),
     AAAA('astromega', ASTROMEGA_6),
-    A('jonckheere', JONCKHEERE),
-    AAAA('jonckheere', JONCKHEERE_6),
     CNAME('*', 'joshuagoodw.in.'),
     CNAME('tools', 'jclgoodwin.gitlab.io.'),
     TXT('@', 'google-site-verification=swE7yOczprWYM5AVzFarzB_Xiy128k5gn3_S9C2Uxls'),
@@ -95,8 +98,8 @@ D('joshuagoodwin.com', REG_NONE, DnsProvider(DNS_CLOUDFLARE),
 );
 
 D('supermarketmarket.co.uk', REG_NONE, DnsProvider(DNS_CLOUDFLARE),
-    A('@', JONCKHEERE),
-    AAAA('@', JONCKHEERE_6),
+    A('@', mongoose),
+    AAAA('@', mongoose_6),
     CNAME('www', 'supermarketmarket.co.uk.'),
     TXT('@', 'google-site-verification=Lo7vOAWM5ruYz4la5EtBOyljWncFU1_jgs4dodt3P98'),
     STANDARD_SPF,
